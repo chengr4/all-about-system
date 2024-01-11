0f8d3448562f4f857beb86a23151429ad74fc788
R4 Cheng <karaburi2023@gmail.com>
2024-01-11 14:18:14 +0800

    update oc

--- solid/README.md
+++ solid/README.md
@@ -13,6 +13,8 @@ Eg. If the class has `logMsg()` and `trackMsg()` (two responsibilities) => move 
 - Software entities (classes, modules, functions, etc.) should be open for extension, but closed for modification
   - Open: We are able to add new things without changing the code inside the function
   - Close: The code changed outside the function should not affect the code inside the function (the code in function should not be modified)
+- 簡單的說就是加 plugin 的概念
+- 目的：增加新功能時，減少修改已經可以運作的程式碼的可能性，以免出現預料之外的 bug
 
 ## "L"iskov Substitution Principle
 
--- solid/deno/o.ts
+++ solid/deno/o.ts
@@ -1,19 +1,35 @@
 // extract item and method
 
 class Item {
+  item: string;
   getLogistic(): string {
-    return 'A City';
+    return this.item;
   }
+  setLogistic(item: string): void {
+    this.item = item;
+  }
+}
+
+// "open" for extension
+// for calculateGood()
+const logisticMap = {
+  "A City": () => 10,
+  "B City": () => 20,
 }
 
 class TransportationFeeCalculator {
 
   // if you keep modifying this method by if-else, it violates the open-closed principle (never close)
-  calculate(item: Item): number {
+  calculateBad(item: Item): number {
     if (item.getLogistic() === 'A City') {
       return 10;
     }
-
+    
     return 20;
+  }
+  
+  // the calculate should be our "closed" interface
+  calculateGood(item: Item): number {
+    return logisticMap[item.getLogistic()]();
   }
 }
